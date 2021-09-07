60731190c9c0d60a136b9656d9a5c949a70a8f5d
Esha Karlekar <esha.karlekar@gmail.com>
2021-09-07 09:57:41 -0400

    lowest level is symbol, not letter; save texttospeech as mp3
    changed it so that instead of letter being the lowest level, the symbols (dots and dashes) are now the lowest level (so there are now three nested lists -- list of symbols = one letter, list of letters = one word, list of words = phrase; save texttospeech as mp3 using Google Cloud

--- morsetotext.py
+++ morsetotext.py
@@ -1,7 +1,7 @@
 import requests
 from requests.exceptions import HTTPError
-
-
+from gtts import gTTS
+from playsound import playsound
 def morseToText(morse_string):
 
 	url = 'http://www.morsecode-api.de/decode?string=' + morse_string
@@ -14,20 +14,31 @@ def morseToText(morse_string):
 '''morse_list format:
    [[letter, letter, letter, etc.], [letter, letter, letter, etc.], ...]]'''
 def toMorseFormat(morse_list):
+	
 	morse_string = ""
 	for word in morse_list:
 		for letter in word:
-			morse_string+= letter
+			sing_letter = ''
+			for symbol in letter:
+				sing_letter += symbol
+			morse_string += sing_letter
 			morse_string+= "%20" # space between letters
 		morse_string+= "%20%20" # space between words
 
 	print(morse_string)
 	return morse_string
 
-morse_list = [["....", ".", ".-..", ".-..", "---"],[".--", "---", ".-.", ".-..", "-.."]] # hello world
+def textToSpeech(text_string, language): # can support English, Hindi, Tamil, French, and German 
+	myobj = gTTS(text=text_string, lang=language, slow=False)
+	myobj.save("texttospeech.mp3")
+	#playsound("texttospeech.mp3")
+
+morse_list = [[['.','.','.','.'], ['.'], ['.', '-', '.', '.'], ['.', '-', '.', '.'], ['-','-','-']],[['.', '-', '-'], ['-','-','-'], ['.','-','.'], ['.','-','.','.'], ['-','.','.']]] # hello world
 
 morse_string = toMorseFormat(morse_list)
 
 text = morseToText(morse_string)
+
+textToSpeech(text, 'en')
 
 print(text)

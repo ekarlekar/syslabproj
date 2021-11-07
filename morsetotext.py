cd0d8ca42355dfeedb2d1927fbcbad065054db6e
Esha Karlekar <esha.karlekar@gmail.com>
2021-11-07 12:27:03 -0500

    Update morsetotext.py

--- morsetotext.py
+++ morsetotext.py
@@ -2,10 +2,13 @@ import requests
 from requests.exceptions import HTTPError
 from gtts import gTTS
 import os
+from tkinter import *
+from tkinter import ttk
+from functools import partial
 
 def toMorseFormat(morse_list):
 	'''
-	Take a morse list as input and returns a string able to be parsed through by a morse-to-text API.
+	Take a morse list as input and returns a string able to be parsed.
 
 	Args:
 		morse_list (3-layer nested list): List of words containing a sublist of letters containing a sublist of symbols (dots and dashes).
@@ -18,25 +21,61 @@ def toMorseFormat(morse_list):
 			for symbol in letter:
 				sing_letter += symbol
 			morse_string += sing_letter
-			morse_string+= "%20" # space between letters
-		morse_string+= "%20%20" # space between words
+			morse_string+= "/" # slash between letters
+		morse_string+= "*" # asterisk between words
+
 
 	print(morse_string)
 	return morse_string
 
 def morseToText(morse_string):
 	'''
-	Take a formatted morse string as input and returns English translation using morsecode-api.de.
+	Take a formatted morse string as input and returns English translation.
 
 	Args:
-		morse_string (string): String to be parsed through, contains one space between letters and two spaces between words.
+		morse_string (string): String to be parsed through, contains '/'' between letters and '*' between words.
 
 	'''
-	url = 'http://www.morsecode-api.de/decode?string=' + morse_string
-	r = requests.get(url)
-	r.raise_for_status()
-	j = r.json()
-	return j.get('plaintext')
+	translations = {
+	'.-': 'a',
+	'-...': 'b',
+	'-.-.': 'c', 
+	'-..': 'd', 
+	'.': 'e', 
+	'..-.': 'f', 
+	'--.': 'g',
+	'....': 'h',
+	'..': 'i',
+	'.---': 'j',
+	'-.-': 'k',
+	'.-..': 'l',
+	'--': 'm',
+	'-.': 'n',
+	'---': 'o',
+	'.--.': 'p',
+	'--.-': 'q',
+	'.-.': 'r',
+	'...': 's', 
+	'-': 't',
+	'..-': 'u', 
+	'...-': 'v',
+	'.--': 'w', 
+	'-..-': 'x', 
+	'-.--': 'y',
+	'--..': 'z',
+	'*': ' ',
+	'': '  '
+	}
+
+	split = morse_string.split('/')
+	translation = ""
+	for letter in split:
+		if '*' in letter:
+			translation += " "
+			letter = letter[1:]
+		translation+=translations[letter]
+	return translation
+
 
 def textToSpeech(text_string, language): 
 	'''
@@ -47,12 +86,12 @@ def textToSpeech(text_string, language):
 
 		language (string): Language of desired output. In theory, can support English, Hindi, Tamil, French, and German but functionality needs to be added. English should be formatted as 'en'.
 
-	'''
+	'''	
 	myobj = gTTS(text=text_string, lang=language, slow=False)
 	myobj.save("texttospeech.mp3")
 	os.system('afplay texttospeech.mp3')
 
-def morseToSpeech(morse_list, language):
+def morseToSpeech(morse_list):
 	'''
 	Takes morse list as input and prints out and speaks aloud English translation.
 
@@ -64,13 +103,36 @@ def morseToSpeech(morse_list, language):
 	'''
 	morse_string = toMorseFormat(morse_list)
 	text = morseToText(morse_string)
-	textToSpeech(text, 'en')
+
+	return text, morse_string
 	print(text)
+def display(root, text, language, morse_string):
+	logo = PhotoImage(file='logologo.png')
+	logo = logo.zoom(10) #with 250, I ended up running out of memory
+	logo = logo.subsample(30)
+	play = PhotoImage(file='playbutton.png')
+	#play = play.zoom(10) #with 250, I ended up running out of memory
+	play = play.subsample(30)
+	morse_string = morse_string.replace('/', '')
+	morse_string = morse_string.replace('*', ' ')
+	frm = ttk.Frame(root, padding=10)
+	frm.grid()
+	ttk.Label(frm, image=logo).grid(column=0, row=0)
+	ttk.Label(frm, text="Morse Code (from blinks): "+morse_string).grid(column=0, row=2)
+	ttk.Label(frm, text="English Translation: "+text).grid(column=0, row=3)
+	frm1 = ttk.Frame(frm, padding=1)
+	ttk.Button(frm, text="Play Sound", width=10, command=partial(textToSpeech, text, language)).grid(column=0, row=4)
+	ttk.Button(frm, text="Quit", width=10, command=root.destroy).grid(column=0, row=5)
+	root.mainloop()
+	return
 
 def main():
+	root = Tk()
+	root.title('Blinks to Speech')
 
 	morse_list = [[['.','.','.','.'], ['.'], ['.', '-', '.', '.'], ['.', '-', '.', '.'], ['-','-','-']],[['.', '-', '-'], ['-','-','-'], ['.','-','.'], ['.','-','.','.'], ['-','.','.']]] # hello world
 	language = 'en'
-	morseToSpeech(morse_list, language)
+	text, morse_string = morseToSpeech(morse_list)
+	display(root, text, language, morse_string)
 
 if __name__ == '__main__': main()

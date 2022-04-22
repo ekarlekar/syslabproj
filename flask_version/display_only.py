a39633d0f89d81ff7fcd87730e69f2c59bb1eb89
atin-can <50777724+atin-can@users.noreply.github.com>
2022-04-22 00:06:52 -0400

    added live updates to Flask server with turboflask

--- flask_version/display_only.py
+++ flask_version/display_only.py
@@ -1,14 +1,31 @@
 from flask import Flask
 from flask import request
+from flask import render_template
+from turbo_flask import Turbo
+import threading
+import time
+import random
 
 app = Flask(__name__)
 
+turbo = Turbo(app)
+
 @app.route("/")
 def main():
-    return "<p>Esha did not do anything</p>"
+    return render_template("index.html")
 
 translation = 0
 @app.context_processor
 def inject_load():
-    translation +=1
+    translation = translation+1
     return {'translation': translation}
+
+def update_load():
+    with app.app_context():
+        while True:
+            time.sleep(5)
+            turbo.push(turbo.replace(render_template('loadtranslation.html'), 'load'))
+
+@app.before_first_request
+def before_first_request():
+    threading.Thread(target=update_load).start()
